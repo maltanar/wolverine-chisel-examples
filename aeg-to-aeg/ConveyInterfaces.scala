279e28b5ec134a051a69fca4680ac286cc8e1a89
Yaman Umuroglu <maltanar@gmail.com>
2015-06-12 17:48:19 +0200

    refactor+expand IF definitions

--- aeg-to-aeg/ConveyInterfaces.scala
+++ aeg-to-aeg/ConveyInterfaces.scala
@@ -4,27 +4,27 @@ import Chisel._
 import Literal._
 import Node._
 
-// command bundle for read/writes to AEG registers
-class RegCommand() extends Bundle {
-  val regID     = UInt(width = 18)
+// command bundle for read/writes to AEG/CSR registers
+class RegCommand(idBits: Int, dataBits: Int) extends Bundle {
+  val regID     = UInt(width = idBits)
   val read      = Bool()
   val write     = Bool()
-  val writeData = UInt(width = 64)
+  val writeData = UInt(width = dataBits)
 
-  override def clone = { new RegCommand().asInstanceOf[this.type] }
+  override def clone = { new RegCommand(idBits, dataBits).asInstanceOf[this.type] }
 }
 
 // register file interface
-class AEGSlaveIF() extends Bundle {
+class RegFileSlaveIF(idBits: Int, dataBits: Int) extends Bundle {
   // register read/write commands
   // the "valid" signal here should be connected to (.read OR .write)
-  val cmd         = ValidIO(new RegCommand()).flip
+  val cmd         = Valid(new RegCommand(idBits, dataBits)).flip
   // returned read data
-  val readData    = ValidIO(UInt(width = 64))
+  val readData    = Valid(UInt(width = dataBits))
   // number of registers
-  val regCount    = UInt(OUTPUT, width = 18)
+  val regCount    = UInt(OUTPUT, width = idBits)
 
-  override def clone = { new AEGSlaveIF().asInstanceOf[this.type] }
+  override def clone = { new RegFileSlaveIF(idBits, dataBits).asInstanceOf[this.type] }
 }
 
 // dispatch slave interface
@@ -32,9 +32,61 @@ class AEGSlaveIF() extends Bundle {
 class DispatchSlaveIF() extends Bundle {
   // instruction opcode
   // note that this interface is defined as stall-valid instead of ready-valid
-  // by Convey, so the read signal should be inverted from stall
-  val instr       = Decoupled(UInt(width = 4)).flip
-  val aeg         = new AEGSlaveIF()
+  // by Convey, so the ready signal should be inverted from stall
+  val instr       = Decoupled(UInt(width = 5)).flip
+  // register file access
+  val aeg         = new RegFileSlaveIF(18, 64)
+  // output for signalling instruction exceptions
+  val exception   = UInt(OUTPUT, width = 16)
 
   override def clone = { new DispatchSlaveIF().asInstanceOf[this.type] }
+}
+
+class TopWrapperInterface() extends Bundle {
+  // dispatch interface
+  val dispInstValid = Bool(INPUT)
+  val dispInstData  = UInt(INPUT, width = 5)
+  val dispRegID     = UInt(INPUT, width = 18)
+  val dispRegRead   = Bool(INPUT)
+  val dispRegWrite  = Bool(INPUT)
+  val dispRegWrData = UInt(INPUT, width = 64)
+  val dispAegCnt    = UInt(OUTPUT, width = 18)
+  val dispException = UInt(OUTPUT, width = 16)
+  val dispIdle      = Bool(OUTPUT)
+  val dispRtnValid  = Bool(OUTPUT)
+  val dispRtnData   = UInt(OUTPUT, width = 64)
+  val disp_stall    = Bool(OUTPUT)
+  // TODO add memory controller ports here
+  // control-status register interface
+  val csrWrValid      = Bool(INPUT)
+  val csrRdValid      = Bool(INPUT)
+  val csrAddr         = UInt(INPUT, 16)
+  val csrWrData       = UInt(INPUT, 64)
+  val csrReadAck      = Bool(OUTPUT)
+  val csrReadData     = UInt(OUTPUT, 64)
+  // misc
+  val aeid            = UInt(INPUT, 4)
+
+  // rename signals
+  def renameSignals() {
+    dispInstValid.setName("disp_inst_vld")
+    dispInstData.setName("disp_inst")
+    dispRegID.setName("disp_aeg_idx")
+    dispRegRead.setName("disp_aeg_rd")
+    dispRegWrite.setName("disp_aeg_wr")
+    dispRegWrData.setName("disp_aeg_wr_data")
+    dispAegCnt.setName("disp_aeg_cnt")
+    dispException.setName("disp_exception")
+    dispIdle.setName("disp_idle")
+    dispRtnValid.setName("disp_rtn_data_vld")
+    dispRtnData.setName("disp_rtn_data")
+    disp_stall.setName("disp_stall")
+    csrWrValid.setName("csr_wr_vld")
+    csrRdValid.setName("csr_rd_vld")
+    csrAddr.setName("csr_address")
+    csrWrData.setName("csr_wr_data")
+    csrReadAck.setName("csr_rd_ack")
+    csrReadData.setName("csr_rd_data")
+    aeid.setName("i_aeid")
+  }
 }
