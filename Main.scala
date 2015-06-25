a550662c748709832fea0ec91852f2ebd74b5c7d
Yaman Umuroglu <maltanar@gmail.com>
2015-06-26 00:05:54 +0200

    an attempt at parametrizing Main.scala for command line module gen

--- Main.scala
+++ Main.scala
@@ -1,16 +1,32 @@
 import Chisel._
 
 object MainObj {
-  def main(args: Array[String]): Unit = {
-    // number of memory ports to instantiate
-    val numMemPorts = 1
+  val defaultArgs: Array[String] = Array("--v")
 
+  def main(args: Array[String]): Unit = {
+
+    val functionMap: Map[String, () => Unit] = Map(
+      "MemSum" -> makeMemSum,
+      "ReadReqGen" -> makeReadReqGen
+    )
+    val moduleName = args(0)
+    println("Building module: " + moduleName)
+
+    functionMap(moduleName)()
+  }
+
+  def makeMemSum(): Unit = {
+    // number of memory ports to instantiate
+    // TODO command line-parametize (weakly, so we get defaults too)
+    val numMemPorts = 1
     // this "fabricator function" is needed due to how modules need to be
     // wrapped, as in Module(new ModuleType()), is implemented in Chisel
-    //def InstPersonality() : Personality = {new AEGtoAEG(numMemPorts)}
     def InstPersonality() : Personality = {new MemSum(numMemPorts)}
+    chiselMain(defaultArgs, () => Module(new PersonalityWrapper(numMemPorts, InstPersonality)))
+  }
 
-    chiselMain(args, () => Module(new PersonalityWrapper(numMemPorts, InstPersonality) ))
-    //chiselMainTest(args, () => Module(new AXIStreamDownsizer(64,16))) { c => new AXIStreamDownsizerTester(c)}
+  def makeReadReqGen(): Unit = {
+    val p = new MemReqParams(48, 64, 4, 1, 8)
+    chiselMain(defaultArgs, () => Module(new ReadReqGen(p, 0) ))
   }
 }
