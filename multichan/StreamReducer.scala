b18d5492ec7a3e148e46a30fe6cf9807789b3b67
Yaman Umuroglu <maltanar@gmail.com>
2015-06-26 17:58:10 +0200

    remove initial value port (compile time) and use bytes instead of elems

--- multichan/StreamReducer.scala
+++ multichan/StreamReducer.scala
@@ -6,16 +6,16 @@ class StreamReducer(w: Int, initVal: Int, fxn: (UInt,UInt)=>UInt) extends Module
   val io = new Bundle {
     val start = Bool(INPUT)
     val finished = Bool(OUTPUT)
-    val initial = UInt(INPUT, width = w )
     val reduced = UInt(OUTPUT, width = w )
-    val count = UInt(INPUT, width = 32)
+    val byteCount = UInt(INPUT, width = 32)
     val streamIn = Decoupled(UInt(width = w)).flip
   }
+  val bytesPerElem = w/8
 
   val sIdle :: sRunning :: sFinished :: Nil = Enum(UInt(), 3)
   val regState = Reg(init = UInt(sIdle))
   val regReduced = Reg(init = UInt(initVal, width = w))
-  val regElemsLeft = Reg(init = UInt(0, 32))
+  val regBytesLeft = Reg(init = UInt(0, 32))
 
   io.finished := Bool(false)
   io.reduced := regReduced
@@ -23,19 +23,19 @@ class StreamReducer(w: Int, initVal: Int, fxn: (UInt,UInt)=>UInt) extends Module
 
   switch(regState) {
       is(sIdle) {
-        regReduced := io.initial
-        regElemsLeft := io.count
+        regReduced := UInt(initVal)
+        regElemsLeft := io.byteCount
 
         when (io.start) { regState := sRunning }
       }
 
       is(sRunning) {
-        when (regElemsLeft === UInt(0)) { regState := sFinished}
+        when (regBytesLeft === UInt(0)) { regState := sFinished}
         .otherwise {
           io.streamIn.ready := Bool(true)
           when (io.streamIn.valid) {
             regReduced := fxn(regReduced, io.streamIn.bits)
-            regElemsLeft := regElemsLeft - UInt(1)
+            regBytesLeft := regBytesLeft - UInt(bytesPerElem)
           }
         }
       }
