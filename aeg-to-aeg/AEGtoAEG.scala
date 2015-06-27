99ee2428e98b8f7ed0f3c43cfa7ae7ee7d7d4e32
Yaman Umuroglu <maltanar@gmail.com>
2015-06-27 15:58:05 +0200

    fix personalities to work with the new RegFile interface

--- aeg-to-aeg/AEGtoAEG.scala
+++ aeg-to-aeg/AEGtoAEG.scala
@@ -2,13 +2,13 @@ import Chisel._
 import ConveyInterfaces._
 
 class AEGtoAEG(numMemPorts: Int) extends Personality(numMemPorts) {
-  // I/O is defined by the base class (Personality)
-
+  val aegRegCount = 4
+  val csrRegCount = 4
   // instantiate and connect main (ops) register file
-  val regFile = Module(new RegFile(4, 18, 64))
+  val regFile = Module(new RegFile(aegRegCount, 18, 64))
   regFile.io.extIF <> io.disp.aeg
   // instantiate and connect the CSR register file
-  val csrRegFile = Module(new RegFile(4, 16, 64))
+  val csrRegFile = Module(new RegFile(csrRegCount, 16, 64))
   csrRegFile.io.extIF <> io.csr
 
   // register to sum values into
@@ -22,14 +22,23 @@ class AEGtoAEG(numMemPorts: Int) extends Personality(numMemPorts) {
   io.disp.instr.ready := Bool(false)
 
   // ops regfile internal write port
-  regFile.io.regWrInd := UInt(2)
-  regFile.io.regWrData := regSum
-  regFile.io.regWrEn := Bool(false)
+  val regIndSum = 2
+  val regIndOpCount = 3
+  def enableWrite(ind: Int) = { regFile.io.regIn(ind).valid := Bool(true)}
+  regFile.io.regIn(regIndSum).bits := regSum
+  regFile.io.regIn(regIndOpCount).bits := regOpCount
 
-  // csr regfile internal write port
-  csrRegFile.io.regWrInd := UInt(0)
-  csrRegFile.io.regWrData := regOpCount
-  csrRegFile.io.regWrEn := Bool(false)
+  // plug ops regfile internal write enables by default (set when needed)
+  for(i <- 0 until aegRegCount) {
+    regFile.io.regIn(i).valid := Bool(false)
+  }
+
+  // plug csr regfile internal write ports, we don't use them in this example
+  for(i <- 0 until csrRegCount) {
+    csrRegFile.io.regIn(i).valid := Bool(false)
+    csrRegFile.io.regIn(i).bits := UInt(0)
+  }
+
 
   // control state machine
   val sIdle :: sRead :: sWrite :: Nil = Enum(UInt(), 3)
@@ -54,10 +63,9 @@ class AEGtoAEG(numMemPorts: Int) extends Personality(numMemPorts) {
       }
 
       is(sWrite) {
-        // write result (into register 2, addr hardcoded)
-        regFile.io.regWrEn := Bool(true)
-        // also update CSR (regOpCount)
-        csrRegFile.io.regWrEn := Bool(true)
+        // write results
+        enableWrite(regIndSum)
+        enableWrite(regIndOpCount)
         // go back to idle
         regState := sIdle
       }
